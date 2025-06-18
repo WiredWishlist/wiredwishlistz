52af5d2ae869633a4ea459b7106dab0aef632c93
WiredWishlist <vava73066@gmail.com>
2025-06-18 17:32:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to WiredWishlist 🔌</h1>
+      <p>This is your affiliate showcase site built with React + Vite.</p>
+    </div>
+  );
+}
+
+export default App;
